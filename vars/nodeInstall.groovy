f7d71c3388b5e85458d84cb6681a43d438afec24
Kanisa Pradhan <02230283.cst@rub.edu.bt>
2025-05-24 17:11:04 +0600

    [fix:nodeinstall]

--- vars/nodeInstall.groovy
+++ vars/nodeInstall.groovy
@@ -12,16 +12,3 @@ def call(String nodeVersion = '18') {
     
     echo "Dependencies installed successfully!"
 }
-    echo "Using ${npmCommand} (package-lock.json ${lockFileExists ? 'found' : 'not found'})"
-    
-    // Use Node.js Docker image to install dependencies
-    sh """
-        docker run --rm \
-            -v \${PWD}/${workDir}:/app \
-            -w /app \
-            node:${nodeVersion}-alpine \
-            ${npmCommand} --only=production
-    """
-    
-    echo "Dependencies installed successfully!"
-}
